9827f0614f2a4d7b8ed7c1d1e0a67005e6c3eb4d
jitu <iamjitu.pradhan@gmail..com>
2021-09-19 15:15:35 +0530

    firstcommit

---
+++ pythonBasic/test.py
@@ -0,0 +1,10 @@
+print("hello")
+
+course = "Python Program";
+print(len(course))
+print(course[0])  # prints the 0th index char
+print(course[-1])  # prints the last char of the string
+print(course[0:3])  # prints Pyt
+print(course[:])  # repeats the string again
+print(course[:3])  # default it takes the 0
+print(course[0:])  # default it takes to the end of the string
